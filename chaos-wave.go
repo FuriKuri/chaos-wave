cc68a98482d326356790999def4fc74142d30308
Theo Pack Test <tf.pack@gmail.com>
2016-09-09 20:03:19 +0200

    List all running containers

---
+++ chaos-wave.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+)
+
+func main() {
+	cli, err := client.NewClient(
+		"unix:///var/run/docker.sock",
+		"v1.22",
+		nil,
+		map[string]string{"User-Agent": "chaos-wave-0.1"})
+	if err != nil {
+		panic(err)
+	}
+
+	options := types.ContainerListOptions{All: true}
+	containers, err := cli.ContainerList(context.Background(), options)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, c := range containers {
+		if c.State == "running" {
+			fmt.Println(c.ID + " " + c.State + " " + c.Status)
+		}
+	}
+}
