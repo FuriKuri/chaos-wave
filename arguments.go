9046c4a4e982bf103cff0c5d28ecb386b4ae7723
Theo Pack Test <tf.pack@gmail.com>
2016-09-11 11:09:21 +0200

    First working version

---
+++ arguments.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+func interval() time.Duration {
+	duration, err := time.ParseDuration(getArgParameter("interval", "10m"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return duration
+}
+
+func duration() time.Duration {
+	duration, err := time.ParseDuration(getArgParameter("duration", "1h"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return duration
+}
+
+func getArgParameter(name string, defaultValue string) string {
+	argsWithoutProg := os.Args[1:]
+	for index, element := range argsWithoutProg {
+		if element == "--"+name {
+			return argsWithoutProg[index+1]
+		}
+	}
+	return defaultValue
+}
--- chaos-wave.go
+++ chaos-wave.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 	"golang.org/x/net/context"
+	"math/rand"
+	"time"
+)
+
+const (
+	imageName = "furikuri/chaos-wave"
 )
 
 func main() {
@@ -14,15 +19,47 @@ func main() {
 		"v1.22",
 		nil,
 		map[string]string{"User-Agent": "chaos-wave-0.1"})
+	randomGen := rand.New(rand.NewSource(time.Now().UnixNano()))
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Duration: %s.\n", duration())
+	fmt.Printf("Interval: %s.\n", interval())
+	endTime := time.Now().Add(duration())
+	for endTime.After(time.Now()) {
+		time.Sleep(interval())
+		stopRandomContainer(cli, randomGen)
+	}
+}
 
+func validContainer(c types.Container) bool {
+	if c.State == "running" && c.Image != imageName {
+		return true
+	}
+	return false
+}
+
+func stopRandomContainer(cli *client.Client, randomGen *rand.Rand) {
+	var validContainers []types.Container
 	for _, c := range containers(cli) {
-		if c.State == "running" {
-			fmt.Println(c.ID + " " + c.State + " " + c.Status)
+		if validContainer(c) {
+			validContainers = append(validContainers, c)
 		}
 	}
+	if len(validContainers) == 0 {
+		fmt.Println("No running container. Nothing to kill :(")
+	}
+	if len(validContainers) > 0 {
+		containerToStop := validContainers[randomGen.Intn(len(validContainers))]
+		fmt.Printf("Stopping container with name '%s' \n", containerToStop.Names[0])
+		stopAndRemoveContainer(containerToStop, cli)
+	}
+}
+
+func stopAndRemoveContainer(container types.Container, cli *client.Client) {
+	cli.ContainerRemove(context.Background(), container.ID, types.ContainerRemoveOptions{
+		Force: true,
+	})
 }
 
 func containers(cli *client.Client) []types.Container {
