1223e382b39b954677f7723287c8bbff19f20aa1
Theo Pack Test <tf.pack@gmail.com>
2016-09-11 09:11:04 +0200

    Refactor

--- chaos-wave.go
+++ chaos-wave.go
@@ -18,15 +18,18 @@ func main() {
 		panic(err)
 	}
 
+	for _, c := range containers(cli) {
+		if c.State == "running" {
+			fmt.Println(c.ID + " " + c.State + " " + c.Status)
+		}
+	}
+}
+
+func containers(cli *client.Client) []types.Container {
 	options := types.ContainerListOptions{All: true}
 	containers, err := cli.ContainerList(context.Background(), options)
 	if err != nil {
 		panic(err)
 	}
-
-	for _, c := range containers {
-		if c.State == "running" {
-			fmt.Println(c.ID + " " + c.State + " " + c.Status)
-		}
-	}
+	return containers
 }
